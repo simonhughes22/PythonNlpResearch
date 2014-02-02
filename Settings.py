d82982f001753db717d744b5ebc8934e444adcb2
hughes-meister <simon.hughes@dice.com>
2014-02-02 12:08:25 -0600

    Fixing Spelling Corrector paths

--- Settings.py
+++ Settings.py
@@ -21,6 +21,7 @@ class Settings(object):
         # Ensure is in linux form, which works for both OS'
         root_path = linux_delim.join( parts[: ix_root + 1] ) + linux_delim
         self.root_path = root_path
+        self.public_data_dir = self.root_path + "/Data/PublicDatasets/"
 
         self.results_directory = "/Users/simon.hughes/Dropbox/Phd/Results/"
         self.data_directory    = "/Users/simon.hughes/Dropbox/Phd/Data/"
@@ -28,7 +29,8 @@ class Settings(object):
         print "Results Dir: "   + self.results_directory
         print "Data Dir:    "   + self.data_directory
         print "Root Dir:    "   + self.root_path
-    
+        print "Public Data: "   + self.public_data_dir
+
 if __name__ == "__main__":
 
     stg = Settings()
@@ -36,5 +38,6 @@ if __name__ == "__main__":
     assert os.path.exists(stg.data_directory),      "Data Directory does not exist"
     assert os.path.exists(stg.results_directory),   "Results Directory does not exist"
     assert os.path.exists(stg.root_path),           "Root Path does not exist"
+    assert os.path.exists(stg.public_data_dir),     "Public Datasets does not exist"
 
     i = raw_input("Press any key...")
