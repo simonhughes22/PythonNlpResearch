c6560b01c366f4fb42fe752ebe182b27490a0df0
Simon Hughes <simon.hughes@dice.com>
2015-07-06 23:20:39 -0500

    Code to store the word2vec vectors as a pickled dictionary

--- Data/Word2Vec_to_file.py
+++ Data/Word2Vec_to_file.py
@@ -38,23 +38,27 @@ def vectors_to_pickled_dict(desired_words, output_file, norm = True, filename = 
     print("Loading Model")
     model = Word2Vec.load_word2vec_format(filename, binary=True)
     print("Loaded")
-    v_words = model.vocab.keys()
 
-    wd2vec = defaultdict(lambda: np.zeros(300))
+    wd2vec = dict()
     if desired_words:
         desired_words = set(desired_words)
+    else:
+        desired_words = model.vocab.keys()
 
-    for i, wd in enumerate(v_words):
+    for i, wd in enumerate(desired_words):
         if i % 1000 == 0:
             print(i)
 
+        wd = remove_non_ascii(wd).replace("  "," ").strip()
         # for phrases
-        wd_key = remove_non_ascii(wd).replace("_", " ").replace("  "," ")
-        if desired_words is None or wd in desired_words:
-            ix = model.vocab[wd].index
+        wd_key = wd.replace(" ", "_")
+        if wd_key in model.vocab:
+            ix = model.vocab[wd_key].index
             vector = model.syn0norm[ix] if norm else model.syn0[ix]
-            wd2vec[wd_key] = vector
+            wd2vec[wd.replace("_"," ").strip()] = vector
 
-    Pickle.dump(wd2vec, output_file)
+    with open(output_file, "w+") as f:
+        Pickle.dump(wd2vec, f)
 
-save_vectors_to_text(None, "/Users/simon.hughes/Documents/Dice Data/GoogleNews-vectors-negative300.txt")
+if __name__ == "__main__":
+    save_vectors_to_text(None, "/Users/simon.hughes/Documents/Dice Data/GoogleNews-vectors-negative300.txt")
