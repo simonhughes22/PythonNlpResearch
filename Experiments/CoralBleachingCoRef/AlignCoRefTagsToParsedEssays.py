c3bb25a56292d1d3bb8eb71b536231f624c0d8c8
Simon Hughes <simonhughes22@hotmail.com>
2018-09-11 20:29:48 -0500

    Create a new file for sharing logic. Add some notes

--- Experiments/CoralBleachingCoRef/AlignCoRefTagsToParsedEssays.py
+++ Experiments/CoralBleachingCoRef/AlignCoRefTagsToParsedEssays.py
@@ -21,6 +21,10 @@ config = get_config(target_folder)
 mem_process_essays = memoize_to_disk(filename_prefix=processed_essay_filename_prefix)(load_process_essays)
 tagged_essays = mem_process_essays(**config)
 print("{0} essays loaded".format(len(tagged_essays)))
+# map parsed essays to essay name
+essay2tagged = {}
+for e in tagged_essays:
+    essay2tagged[e.name.split(".")[0]] = e
 
 coref_root = root_folder + "CoReference/"
 coref_folder = coref_root + partition
@@ -82,11 +86,6 @@ def parse_stanfordnlp_tagged_essays(coref_files):
     return essay2tagged
 
 essay2coref_tagged = parse_stanfordnlp_tagged_essays(coref_files)
-
-# map parsed essays to essay name
-essay2tagged = {}
-for e in tagged_essays:
-    essay2tagged[e.name.split(".")[0]] = e
 assert len(essay2tagged) == len(essay2coref_tagged)
 
 failed_cnt = 0
@@ -101,23 +100,23 @@ for ename, coref_essay in essay2coref_tagged.items():
     assert ename in essay2tagged
     tagged_essay = essay2tagged[ename]
 
-    wds1 = []
+    wds_tagd = []
     taggedwd2sentixs = {}
     replacement_sent2wdix = {}
     for sent_ix, sent in enumerate(tagged_essay.sentences):
         for wd_ix, (wd, tags) in enumerate(sent):
-            taggedwd2sentixs[len(wds1)] = (sent_ix, wd_ix)
+            taggedwd2sentixs[len(wds_tagd)] = (sent_ix, wd_ix)
             if wd == "\'\'":
                 wd = "\""
-            wds1.append((wd, tags))
+            wds_tagd.append((wd, tags))
 
-    wds2 = []
+    wds_coref = []
     mentions = []
     for sent_ix, sent in enumerate(coref_essay):
         current_mention = ""
         mention_ixs = set()
         for wd_ix, (wd, tag_dict) in enumerate(sent):
-            wds2.append((wd, tag_dict))
+            wds_coref.append((wd, tag_dict))
             if COREF_PHRASE not in tag_dict:
                 if current_mention != "":
                     mentions.append((current_mention, mention_ixs))
@@ -130,77 +129,77 @@ for ename, coref_essay in essay2coref_tagged.items():
                     current_mention = ""
                     mention_ixs = set()
                 current_mention = phrase
-                mention_ixs.add(len(wds2) - 1)
+                mention_ixs.add(len(wds_coref) - 1)
         if current_mention != "":
             mentions.append((current_mention, mention_ixs))
 
     # if len(mentions) == 0:
     #     continue
 
-    ix_a, ix_b = 0, 0
-    wd1ix_wd2ix = {}
-    wd2ix_wd1ix = {}
+    ix_tagd, ix_coref = 0, 0
+    ixtagd_2_ixcoref = {}
+    ixcoref_2_ixtagd = {}
 
-    while ix_a < (len(wds1) - 1) and ix_b < (len(wds2) - 1):
-        a, atags = wds1[ix_a]
-        b, btag_dict = wds2[ix_b]
+    while ix_tagd < (len(wds_tagd) - 1) and ix_coref < (len(wds_coref) - 1):
+        wd_tagd, atags = wds_tagd[ix_tagd]
+        wd_coref, btag_dict = wds_coref[ix_coref]
 
-        if a == b or a == "cannot" and b =="can":
-            wd1ix_wd2ix[ix_a] = ix_b
-            wd2ix_wd1ix[ix_b] = ix_a
-            ix_a += 1
-            ix_b += 1
-
+        if wd_tagd == wd_coref or wd_tagd == "cannot" and wd_coref == "can":
+            ixtagd_2_ixcoref[ix_tagd]  = ix_coref
+            ixcoref_2_ixtagd[ix_coref] = ix_tagd
+            ix_tagd  += 1
+            ix_coref += 1
         else:
             # look ahead in wds2 for item that matches next a
             found_match = False
-            for offseta, (aa, atags) in enumerate(wds1[ix_a: ix_a + 1 + SCAN_LENGTH]):
-                for offsetb, (bb, bb_tag_dict) in enumerate(wds2[ix_b:ix_b + 1 + SCAN_LENGTH]):
+            for offseta, (aa, atags) in enumerate(wds_tagd[ix_tagd: ix_tagd + 1 + SCAN_LENGTH]):
+                for offsetb, (bb, bb_tag_dict) in enumerate(wds_coref[ix_coref:ix_coref + 1 + SCAN_LENGTH]):
                     if aa == bb:
                         if offseta == offsetb:
-                            for i in range(ix_a,ix_a+offseta):
-                                if i not in wd1ix_wd2ix:
-                                    wd1ix_wd2ix[i] = i
+                            for i in range(ix_tagd, ix_tagd + offseta):
+                                if i not in ixtagd_2_ixcoref:
+                                    ixtagd_2_ixcoref[i] = i
 
-                        ix_a = ix_a + offseta
-                        ix_b = ix_b + offsetb
-                        wd1ix_wd2ix[ix_a] = ix_b
-                        wd2ix_wd1ix[ix_b] = ix_a
+                        ix_tagd  = ix_tagd + offseta
+                        ix_coref = ix_coref + offsetb
+                        ixtagd_2_ixcoref[ix_tagd] = ix_coref
+                        ixcoref_2_ixtagd[ix_coref] = ix_tagd
                         found_match = True
                         break
                 if found_match:
                     break
             if not found_match:
-                print("Failed: " + ename, a, b, ix_a, len(wds1), ix_b, len(wds2))
+                print("Failed: " + ename, wd_tagd, wd_coref, ix_tagd, len(wds_tagd), ix_coref, len(wds_coref))
                 failed_cnt += 1
                 break
 
+    # replace the corefs/amaphors with their antecedents
     for mention, ixs in mentions:
         first_ix = min(ixs)
         is_fuzzy = False
-        if first_ix not in wd2ix_wd1ix:
-            while first_ix > 0 and first_ix not in wd2ix_wd1ix:
+        if first_ix not in ixcoref_2_ixtagd:
+            while first_ix > 0 and first_ix not in ixcoref_2_ixtagd:
                 first_ix -= 1
-            if first_ix not in wd2ix_wd1ix:
+            if first_ix not in ixcoref_2_ixtagd:
                 e_first_wd_ix = 0
             # one past last matching index
             else:
-                e_first_wd_ix = min(len(wds1) - 1, wd2ix_wd1ix[first_ix] + 1)
+                e_first_wd_ix = min(len(wds_tagd) - 1, ixcoref_2_ixtagd[first_ix] + 1)
             is_fuzzy = True
         else:
-            e_first_wd_ix = wd2ix_wd1ix[first_ix]
+            e_first_wd_ix = ixcoref_2_ixtagd[first_ix]
 
         last_ix = max(ixs)
-        if last_ix not in wd2ix_wd1ix:
-            while last_ix < len(wds2) and last_ix not in wd2ix_wd1ix:
+        if last_ix not in ixcoref_2_ixtagd:
+            while last_ix < len(wds_coref) and last_ix not in ixcoref_2_ixtagd:
                 last_ix += 1
-            if last_ix not in wd2ix_wd1ix:
-                e_last_wd_ix = len(wds1) - 1
+            if last_ix not in ixcoref_2_ixtagd:
+                e_last_wd_ix = len(wds_tagd) - 1
             else:
-                e_last_wd_ix = max(0, wd2ix_wd1ix[last_ix] - 1)
+                e_last_wd_ix = max(0, ixcoref_2_ixtagd[last_ix] - 1)
             is_fuzzy = True
         else:
-            e_last_wd_ix = wd2ix_wd1ix[last_ix]
+            e_last_wd_ix = ixcoref_2_ixtagd[last_ix]
 
         replacement = []
         all_tags = set()
