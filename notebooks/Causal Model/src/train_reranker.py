b180acef3b4b42e19373acbf7df79a13d64e1031
Simon Hughes <simonhughes22@hotmail.com>
2019-06-11 08:39:01 +0100

    Start to refactor the code to capture the metrics in mongo

--- notebooks/Causal Model/src/train_reranker.py
+++ notebooks/Causal Model/src/train_reranker.py
@@ -8,6 +8,8 @@ from MIRA import CostSensitiveMIRA
 from evaluation import add_cr_labels
 from results_procesor import ResultsProcessor
 from train_parser import get_label_data_essay_level, essay_to_crels
+from wordtagginghelper import merge_dictionaries
+
 
 def train_instance(parser_input, model):
     model.train(best_feats=parser_input.opt_features, other_feats_array=parser_input.other_features_array)
@@ -44,10 +46,10 @@ def evaluate_ranker(model, xs, essay2crels, ys_bytag, set_cr_tags):
     # df = get_micro_metrics(metrics_to_df(mean_metrics))
 
     micro_f1 = mean_metrics["MICRO_F1"]["f1_score"]
-    return micro_f1 #, ys_bytag, pred_ys_bytag
+    return micro_f1, ys_bytag, pred_ys_bytag
 
 def train_model(model, xs_train, xs_test, name2essay, set_cr_tags, max_epochs=30, early_stop_iters=8,
-                train_instance_fn=train_instance, verbose=True, early_stopping=True):
+                train_instance_fn=train_instance, verbose=True, early_stopping=True, return_metrics=False):
 
     test_accs = [-1]
     best_model = None
@@ -63,6 +65,8 @@ def train_model(model, xs_train, xs_test, name2essay, set_cr_tags, max_epochs=30
     essay2crels_train = essay_to_crels(train_essays, set_cr_tags)
     essay2crels_test = essay_to_crels(test_essays,   set_cr_tags)
 
+    train_ys_bytag, train_pred_ys_bytag, test_ys_bytag,  test_pred_ys_bytag = None, None, None, None
+
     xs_train_copy = list(xs_train)
     best_iterations = -1
     for i in range(max_epochs):
@@ -71,26 +75,34 @@ def train_model(model, xs_train, xs_test, name2essay, set_cr_tags, max_epochs=30
             if len(parser_input.other_parses) > 0:
                 train_instance_fn(parser_input, model)
 
-        # train_accuracy, _,_ = evaluate_ranker(model, xs_train, essay2crels_train, ys_by_tag_train, set_cr_tags)
-        # test_accuracy,_,_   = evaluate_ranker(model, xs_test, essay2crels_test, ys_by_tag_test, set_cr_tags)
-        train_accuracy = evaluate_ranker(model, xs_train, essay2crels_train, ys_by_tag_train, set_cr_tags)
-        test_accuracy  = evaluate_ranker(model, xs_test, essay2crels_test, ys_by_tag_test, set_cr_tags)
+        train_f1, tmp_train_ys_bytag, tmp_train_pred_ys_bytag = evaluate_ranker(model, xs_train, essay2crels_train, ys_by_tag_train, set_cr_tags)
+        test_f1,  tmp_test_ys_bytag,  tmp_test_pred_ys_bytag  = evaluate_ranker(model, xs_test, essay2crels_test, ys_by_tag_test, set_cr_tags)
+
         if verbose:
             print("Epoch: {epoch} Train Accuracy: {train_acc:.4f} Test Accuracy: {test_acc:.4f}".format(
-                epoch=i, train_acc=train_accuracy, test_acc=test_accuracy))
+                epoch=i, train_acc=train_f1, test_acc=test_f1))
 
-        if not early_stopping or (test_accuracy > max(test_accs)):
+        if not early_stopping or (test_f1 > max(test_accs)):
             best_model = model.clone()
             num_declining_acc = 0
-            best_test_accuracy = test_accuracy
+            best_test_accuracy = test_f1
             best_iterations = i+1
+
+            train_ys_bytag, train_pred_ys_bytag, test_ys_bytag, test_pred_ys_bytag = \
+                tmp_train_ys_bytag, tmp_train_pred_ys_bytag, tmp_test_ys_bytag,  tmp_test_pred_ys_bytag
         else:
             num_declining_acc += 1
             if num_declining_acc >= early_stop_iters:
                 break
-        test_accs.append(test_accuracy)
+
+        test_accs.append(test_f1)
     if verbose:
         print("Best Test Acc: {acc:.4f}".format(acc=max(test_accs)))
+
+    # If return metrics, then return everything
+    if return_metrics:
+        return best_model, best_test_accuracy, best_iterations, train_ys_bytag, train_pred_ys_bytag, test_ys_bytag, test_pred_ys_bytag
+
     return best_model, best_test_accuracy, best_iterations
 
 def shuffle_split_dict(dct, train_pct):
@@ -101,19 +113,37 @@ def shuffle_split_dict(dct, train_pct):
     return dict(train_items), dict(test_items)
 
 def train_model_fold(xs_train, xs_test, name2essay, C, pa_type, loss_type, max_update_items, set_cr_tags, initial_weight):
-    mdl = CostSensitiveMIRA(C=C, pa_type=pa_type, loss_type=loss_type, max_update_items=max_update_items,
-                            initial_weight=initial_weight)
-    best_mdl, f1, best_iter = train_model(mdl, xs_train=xs_train, xs_test=xs_test, name2essay=name2essay,
-                                           max_epochs=20, early_stop_iters=5, set_cr_tags=set_cr_tags,
-                                           train_instance_fn=train_cost_sensitive_instance, verbose=False)
-    return f1
+
+    mdl = CostSensitiveMIRA(
+        C=C, pa_type=pa_type, loss_type=loss_type, max_update_items=max_update_items, initial_weight=initial_weight)
+
+    return train_model(mdl, xs_train=xs_train, xs_test=xs_test, name2essay=name2essay,
+            max_epochs=20, early_stop_iters=5, set_cr_tags=set_cr_tags, train_instance_fn=train_cost_sensitive_instance,
+            verbose=False, return_metrics=True, early_stopping=False)
 
 
 def train_model_parallel(cv_folds, name2essay, C, pa_type, loss_type, max_update_items, set_cr_tags, initial_weight):
     try:
-        f1s = Parallel(n_jobs=len(cv_folds))(
+        results = Parallel(n_jobs=len(cv_folds))(
             delayed(train_model_fold)(train, test, name2essay, C, pa_type, loss_type, max_update_items, set_cr_tags, initial_weight)
             for (train, test) in cv_folds)
+
+
+        cv_sent_td_ys_by_tag, cv_sent_td_predictions_by_tag = defaultdict(list), defaultdict(list)
+        cv_sent_vd_ys_by_tag, cv_sent_vd_predictions_by_tag = defaultdict(list), defaultdict(list)
+
+        f1s = []
+        for tpl in results:
+            best_model, best_test_f1, best_iterations, train_ys_bytag, train_pred_ys_bytag, test_ys_bytag, test_pred_ys_bytag = tpl
+            f1s.append(best_test_f1)
+
+            merge_dictionaries(train_ys_bytag, cv_sent_td_ys_by_tag)
+            merge_dictionaries(test_ys_bytag,  cv_sent_vd_ys_by_tag)
+
+            merge_dictionaries(train_pred_ys_bytag, cv_sent_td_predictions_by_tag)
+            merge_dictionaries(test_pred_ys_bytag,  cv_sent_vd_predictions_by_tag)
+
         return np.mean(f1s)
+
     except KeyboardInterrupt:
         print("Process stopped by user")
